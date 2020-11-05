afcdd8e836a96248a200acaa8ca4a2d90b1314b0
yota-matsushita <yotabattu@hotmail.com>
2020-11-05 18:59:11 +0900

    ユーザー登録の表示、エラー文の表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def index
-  end
-  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
