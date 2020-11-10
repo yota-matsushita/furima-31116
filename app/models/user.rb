88cf900a103c0eff0ca71f57c18ad9c1eba1375a
yota-matsushita <yotabattu@hotmail.com>
2020-11-10 11:25:16 +0900

    ユーザー管理機能　修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,13 @@ class User < ApplicationRecord
             format: { with: /\A[a-zA-Z0-9]+\z/ },
             presence: true, uniqueness: true
   validates :nickname, presence: true
-  validates :lastname,
-            format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ },
-            presence: true
-  validates :firstname,
-            format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ },
-            presence: true
-  validates :lastname_kana,
-            format: { with: /\A([ァ-ン]|ー)+\z/ },
-            presence: true
-  validates :firstname_kana,
-            format: { with: /\A([ァ-ン]|ー)+\z/ },
-            presence: true
+  with_options format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }, presence: true do
+    validates :lastname
+    validates :firstname
+  end
+  with_options format: { with: /\A([ァ-ン]|ー)+\z/ }, presence: true do
+    validates :lastname_kana
+    validates :firstname_kana
+  end
   validates :birthday, presence: true
 end
